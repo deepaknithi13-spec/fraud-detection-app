75f4a2d0f506d700857b5987e29a7aa5f3fc75ce
Iza Bhanu B <deepaknithi13@gmail.com>
2026-04-14 16:00:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import streamlit as st
+# --- LOGIN SYSTEM ---
+username = st.text_input("Username")
+password = st.text_input("Password", type="password")
+
+if username != "admin" or password != "1234":
+    st.warning("Enter correct login details")
+    st.stop()
 st.markdown("""
     <style>
     body {
